cb5ab7796258a2a94db2d8d3d2f29562ac05e370
mihlali <mazombamihlali@gmail.com>
2018-10-22 12:43:42 +0200

    Update WelcomeController.java

--- src/main/java/com/openshift/springboot/webapp/SpringStart/WelcomeController.java
+++ src/main/java/com/openshift/springboot/webapp/SpringStart/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 
-    @RequestMapping("/welcome")
+    @RequestMapping("welcome")
     public String welcome() {
 
         return "welcome";
