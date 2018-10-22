481af844edbde86e08380e8474661518cd6b22d2
mihlali <mazombamihlali@gmail.com>
2018-10-22 15:53:35 +0200

    fix

--- src/main/java/com/openshift/springboot/webapp/SpringStart/WebApplication.java
+++ src/main/java/com/openshift/springboot/webapp/SpringStart/WebApplication.java
@@ -1,13 +1,21 @@
 package com.openshift.springboot.webapp.SpringStart;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-public class WebApplication extends WebMvcConfigurerAdapter {
+public class WebApplication extends SpringBootServletInitializer {
 
    public static void main(String[] args) {
       SpringApplication.run(WebApplication.class, args);
+   }
+   
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+       return application.sources(WebApplication.class);
    }
 }
