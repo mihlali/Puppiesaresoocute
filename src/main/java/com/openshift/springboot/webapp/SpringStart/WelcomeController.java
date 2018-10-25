9b46299f9ccb8558007ebf9e1ca3698205f8ef9a
mihlali <mazombamihlali@gmail.com>
2018-10-25 09:38:38 +0200

    changes to run the springboot app on openshift

--- src/main/java/com/openshift/springboot/webapp/SpringStart/WelcomeController.java
+++ src/main/java/com/openshift/springboot/webapp/SpringStart/WelcomeController.java
@@ -1,12 +1,15 @@
 package com.openshift.springboot.webapp.SpringStart;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+
 public class WelcomeController {
 
-    @RequestMapping("welcome")
+    @RequestMapping("/welcome")
     public String welcome() {
 
         return "welcome";
